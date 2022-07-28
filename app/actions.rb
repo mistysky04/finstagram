0bfed4d3230800b98825edc5c4e8900b9fab7d56
Hedie <headeedoo@gmail.com>
2022-07-28 00:35:05 +0000

    W4D2

--- app/actions.rb
+++ app/actions.rb
@@ -3,3 +3,29 @@ get '/' do
     @finstagram_posts = FinstagramPost.order(created_at: :desc)
     erb(:index)
 end 
+
+get '/signup' do        #if a user navigates to the path "/signup".
+    @user = User.new    #setup empty @user object
+    erb(:signup)        #render "app/view/signup.erb"
+end
+
+post '/signup' do 
+    
+    # grab user input values from params
+    email = params[:email]
+    avatar_url = params[:avatar_url]
+    username = params[:username]
+    password = params[:password]
+
+    #instantiate and save a User
+    @user = User.new({email: email, avatar_url: avatar_url, username: username, password: password})
+        
+    #if user validations pass and user is saved
+    if @user.save
+
+        "User #{username} saved!"
+
+    else
+        erb(:signup)
+    end
+end
